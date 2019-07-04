a9ac62470125f26e2692187e75c9e6675e8b7d17
ryagomes <ryagomes>
2019-07-04 19:12:54 -0400

    mysql connection

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ python application.py
 or  
 export FLASK_APP=application.py  
 flask run  
+
+
+https://pymysql.readthedocs.io/en/latest/index.html  
+https://github.com/PyMySQL/PyMySQL  
+https://www.python.org/dev/peps/pep-0249/#cursor-objects  
--- application.py
+++ application.py
@@ -1,9 +1,23 @@
 from flask import Flask
+import pymysql.cursors
+
 app = Flask(__name__)
+
+connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
+                             user='cs340_gomesr',
+                             password='8619',
+                             db='cs340_gomesr',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def hello_world():
-    return '<h3>Hello CS 340 Databses!!</h3>'
+    with connection.cursor() as cursor:
+        sql='select * from bsg_cert_people'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    # return '<h3>' + str(result[1]['cid']) + '</h3>'
+    return '<h3>' + str(result) + '</h3>'
 
 
 if __name__=='__main__':
