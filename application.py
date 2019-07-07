16756e55a08fc867f60e07a9b3c8ea41bcc10ff4
Kelley Reynolds <kelley@insidesystems.net>
2019-07-07 07:39:39 -0400

    Do not reference bsg_stuff

--- application.py
+++ application.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    with connection.cursor() as cursor:
-        sql='select * from bsg_cert_people'
-        cursor.execute(sql)
-        result = cursor.fetchall()
-    # return '<h3>' + str(result[1]['cid']) + '</h3>'
-    return '<h3>' + str(result) + '</h3>'
+    return '<h3>Hello, world</h3>'
 
 @app.route('/diagnostic')
 def diagnostic():
