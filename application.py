6d423bbdf284aa125b2bb8f26598918c3524b41d
Kelley Reynolds <kelley@insidesystems.net>
2019-07-07 06:32:10 -0400

    Change database import

--- application.py
+++ application.py
@@ -1,23 +1,6 @@
 from flask import Flask
-import pymysql.cursors
-import yaml
-
+from database import *
 app = Flask(__name__)
-
-config = {}
-with open("config/database.yml", 'r') as stream:
-    try:
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-        exit(1)
-
-connection = pymysql.connect(host=config['host'],
-                         user=config['user'],
-                         password=config['password'],
-                         db=config['db'],
-                         charset=config['charset'],
-                         cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def hello_world():
@@ -28,6 +11,13 @@ def hello_world():
     # return '<h3>' + str(result[1]['cid']) + '</h3>'
     return '<h3>' + str(result) + '</h3>'
 
+@app.route('/diagnostic')
+def diagnostic():
+    with connection.cursor() as cursor:
+        sql='SELECT * FROM diagnostic;'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    return '<h3>' + str(result) + '</h3>'
 
 if __name__=='__main__':
     app.run(port=8619, debug=True)
---
+++ database.py
@@ -0,0 +1,19 @@
+import pymysql.cursors
+import yaml
+
+# Read in the configuration file
+config = {}
+with open("config/database.yml", 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
+
+# Create a connection to MySQL
+connection = pymysql.connect(host=config['host'],
+                            user=config['user'],
+                            password=config['password'],
+                            db=config['db'],
+                            charset=config['charset'],
+                            cursorclass=pymysql.cursors.DictCursor)
